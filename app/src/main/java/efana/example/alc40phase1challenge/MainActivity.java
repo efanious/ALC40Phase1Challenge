2d29d0d13777314132897e9fa55c689710927e9f
Efana Usua <usuaefana@gmail.com>
2019-07-10 20:27:26 +0100

    created all the activites and added the click listeners for the buttons

--- app/src/main/java/efana/example/alc40phase1challenge/MainActivity.java
+++ app/src/main/java/efana/example/alc40phase1challenge/MainActivity.java
@@ -1,13 +1,41 @@
 package efana.example.alc40phase1challenge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mButton1;
+    private Button mButton2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton1 = findViewById(R.id.button);
+        mButton2 = findViewById(R.id.button2);
+
+
+        mButton1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AboutALCActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MyProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
