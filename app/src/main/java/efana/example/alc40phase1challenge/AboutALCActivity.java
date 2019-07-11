8fbe2279f0d0e69bd4d162fb01b89188fda39ddd
Efana Usua <usuaefana@gmail.com>
2019-07-11 08:39:42 +0100

    Done with activity B, fixed webView issue

--- app/src/main/java/efana/example/alc40phase1challenge/AboutALCActivity.java
+++ app/src/main/java/efana/example/alc40phase1challenge/AboutALCActivity.java
@@ -1,13 +1,36 @@
 package efana.example.alc40phase1challenge;
 
+import android.net.http.SslError;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class AboutALCActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+    String url = "https://www.andela.com/alc/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_alc);
+
+        myWebView = (WebView) findViewById(R.id.alcWebView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+
+        myWebView.loadUrl(url);
+
+
     }
+
 }
